8657c2d67e9798f00ec90d86ff52c6cfda64c7a4
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-10 08:45:44 +0700

    Update ShapeTest.java because Shape is an abstract class from now

--- src/shape_obj/ShapeTest.java
+++ src/shape_obj/ShapeTest.java
@@ -2,10 +2,20 @@ package shape_obj;
 
 public class ShapeTest {
     public static void main(String[] args) {
-        Shape shape = new Shape();
+        Shape shape = new Shape(){
+            @Override
+            public double getArea() {
+                return 0;
+            }
+        };
         System.out.println(shape);
 
-        shape = new Shape("red", false);
+        shape = new Shape("red", false){
+            @Override
+            public double getArea() {
+                return 0;
+            }
+        };
         System.out.println(shape);
     }
 }
